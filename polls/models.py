abd2f4cb921c444b45e45f44ed3cf520bf54abee
vanhong_office <vanhongkao@gmail.com>
2017-03-10 16:06:01 +0800

    update test case

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,16 @@
 from __future__ import unicode_literals
 
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+	def was_published_recently(self):
+		now = timezone.now()
+		return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= now
 	def __unicode__(self):
 		return self.question_text
 
