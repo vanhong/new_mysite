3116538aadc773a75e29fa9330d4bb8e624e6642
vanhong_office <vanhongkao@gmail.com>
2017-05-08 16:45:52 +0800

    add all

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
 	fields = ['pub_date', 'question_text']
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
--- polls/urls.py
+++ polls/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
 	# ex: /polls/
-	url(r'^$', views.IndexView.as_view(), name='index'),
+	url(r'^$', views.index, name='index'),
 	# ex: /polls/5/
-	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 	# ex: /polls/5/results/
 	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 	# ex: /polls/5/votes/
